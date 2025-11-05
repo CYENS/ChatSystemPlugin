54656d9551f9edeafe1ad7a3c6531df0744747bb
Chrysostomos Chadjiminas <chadjiminas.chrysostomos@gmail.com>
2025-11-05 10:09:01 +0200

    improve chat ui

---
+++ Source/ChatSystem/Private/Data/ChatMessageDataObject.cpp
@@ -0,0 +1,14 @@
+﻿
+#include "Data/ChatMessageDataObject.h"
+
+#include "Data/ChatMessage.h"
+
+FChatMessage UChatMessageDataObject::GetChatMessage() const
+{
+	return ChatMessage;
+}
+
+void UChatMessageDataObject::SetChatMessage(const FChatMessage& Message)
+{
+	ChatMessage = Message;
+}
---
+++ Source/ChatSystem/Public/Data/ChatMessageDataObject.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ChatMessage.h"
+#include "UObject/Object.h"
+#include "ChatMessageDataObject.generated.h"
+
+
+/**
+ * A thin wrapper around a chat message to be used by ListView Entries.
+ */
+UCLASS(Blueprintable)
+class CHATSYSTEM_API UChatMessageDataObject : public UObject
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadOnly, Category="Default", meta=(ExposeOnSpawn=true, AllowPrivateAccess=true))
+	FChatMessage ChatMessage;
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Default")
+	FChatMessage GetChatMessage() const;
+	
+	UFUNCTION(BlueprintCallable, Category="Default")
+	void SetChatMessage(const FChatMessage& Message);
+};
